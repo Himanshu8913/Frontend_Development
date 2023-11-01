fa1e0f3bbe5d12ad898a0e670d165804705e7b83
Himanshu Gupta <himanshugupta558@gmail.com>
2023-11-02 01:35:36 +0530

    more cases on object with ToPrimitive and ToNumber

--- JAVASCRIPT/Coercion/toPrimitiveDemo.js
+++ JAVASCRIPT/Coercion/toPrimitiveDemo.js
@@ -12,3 +12,15 @@
 let obj = {};
 
 console.log(10 - obj); // Number -> obj.valueOf() -> object, toString() -> [object Object] -> string -> string [object Object] to Number -> NaN
+
+let obj1 = {x: 9, y: 8};
+console.log(100 - obj1); // NaN
+
+let obj2 = {x: 7, valueOf() { return 99}};
+console.log(100 - obj2); // 1
+
+let obj3 = {x: 7, toString() { return 88}};
+console.log(100 - obj3); // 12
+
+let obj4 = {x: 7, toString() { return {}}};
+console.log(100 - obj4); // TypeError
