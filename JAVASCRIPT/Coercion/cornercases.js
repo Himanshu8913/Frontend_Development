b016f621ae9462fccd6918aba611332ac4f9149f
Himanshu Gupta <himanshugupta558@gmail.com>
2023-11-02 16:55:29 +0530

    added more corner cases

--- JAVASCRIPT/Coercion/cornercases.js
+++ JAVASCRIPT/Coercion/cornercases.js
@@ -14,3 +14,12 @@ console.log("" + [1,2,null,4]); // 1,2,,4
 // ToNumber
 console.log(0 - "10"); // -10
 console.log(0 - "O10"); // NaN
+
+console.log(0 - 010); // -8 => 010 convrt to octal number
+console.log(0 - "0xb"); //-11 => 0xb convert to hexadecimal number
+console.log(0 - 0xb); //-11
+
+console.log(1 - []); // 1 coz [] becomes 0
+console.log([""] - 1); // -1 coz [""] becomes 0
+
+console.log(["0"] - 1); // -1
