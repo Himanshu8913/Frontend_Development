2a5addf2c8e859415cd7578d464f2754fd552a7a
Himanshu Gupta <himanshugupta558@gmail.com>
2023-11-27 02:56:01 +0530

    added simple event listner

--- JAVASCRIPT/ToDo-Project/index.js
+++ JAVASCRIPT/ToDo-Project/index.js
@@ -1,1 +1,7 @@
 console.log("Welcome to my todo app");
+
+let getTodosButton = document.getElementById('get-todos');
+// registration of event listner
+getTodosButton.addEventListener('click', () => {
+    console.log("clicked");
+})
