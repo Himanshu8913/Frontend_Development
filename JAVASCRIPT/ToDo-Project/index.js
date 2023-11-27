0dd4c8f025fdeae65f7a7a68cce226353cccf5f4
Himanshu Gupta <himanshugupta558@gmail.com>
2023-11-27 11:32:32 +0530

    alternative way to register events

--- JAVASCRIPT/ToDo-Project/index.js
+++ JAVASCRIPT/ToDo-Project/index.js
@@ -2,15 +2,10 @@ console.log("Welcome to my todo app");
 
 let getTodosButton = document.getElementById('get-todos');
 // registration of event listner
-getTodosButton.addEventListener("click", () => {
+// getTodosButton.addEventListener("click", () => {
+//     console.log("clicked");
+// });
+
+getTodosButton.onclick = () => {
     console.log("clicked");
-});
-
-getTodosButton.addEventListener("mouseover", handler)
-getTodosButton.addEventListener("mouseout", () => {
-    console.log("out of button");
-})
-
-function handler() {
-    console.log("mouse hovered!!!");
 }
