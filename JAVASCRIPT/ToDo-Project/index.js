d0ca20860453f3201ecdd4ab104c558649eb5e68
Himanshu Gupta <himanshugupta558@gmail.com>
2023-11-27 11:29:56 +0530

    added code for mouseover & mouseout events

--- JAVASCRIPT/ToDo-Project/index.js
+++ JAVASCRIPT/ToDo-Project/index.js
@@ -2,6 +2,15 @@ console.log("Welcome to my todo app");
 
 let getTodosButton = document.getElementById('get-todos');
 // registration of event listner
-getTodosButton.addEventListener('click', () => {
+getTodosButton.addEventListener("click", () => {
     console.log("clicked");
+});
+
+getTodosButton.addEventListener("mouseover", handler)
+getTodosButton.addEventListener("mouseout", () => {
+    console.log("out of button");
 })
+
+function handler() {
+    console.log("mouse hovered!!!");
+}
