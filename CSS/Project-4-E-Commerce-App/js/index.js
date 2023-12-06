47a9057a7b81ccb959e156f9fd3156f76fad2b2a
Himanshu Gupta <himanshugupta558@gmail.com>
2023-12-07 00:51:48 +0530

    fetching category data from fakestore api

---
+++ CSS/Project-4-E-Commerce-App/js/index.js
@@ -0,0 +1,25 @@
+console.log("js loaded successfully");
+
+async function fetchCategories() {
+    // every async function return a promise
+    const response = await fetch("https://fakestoreapi.com/products/categories");
+    const data = await response.json();
+    // console.log(data);
+    return data;
+}
+
+async function populateCategories() {
+    const categories = await fetchCategories();
+    const categoryList = document.getElementById("categoryList");
+    categories.forEach(category => {
+        const categoryHolder = document.createElement("div");
+        const categoryLink = document.createElement("a");
+        categoryLink.href = "#";
+        categoryLink.textContent = category;
+        categoryHolder.classList.add("category-item", "d-flex", "flex-row", "justify-content-center", "align-items-center",);
+        categoryHolder.appendChild(categoryLink);
+        categoryList.appendChild(categoryHolder);
+    });
+}
+
+populateCategories();
