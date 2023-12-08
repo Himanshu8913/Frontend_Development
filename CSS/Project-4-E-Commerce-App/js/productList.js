22ffac5cfdede6bd6180ad701fe63fce6028eef4
Himanshu Gupta <himanshugupta558@gmail.com>
2023-12-08 16:05:33 +0530

    populating product list with all product details

--- CSS/Project-4-E-Commerce-App/js/productList.js
+++ CSS/Project-4-E-Commerce-App/js/productList.js
@@ -5,6 +5,39 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(response.data);
         return response.data;
     }
-    fetchProducts();
+
+    async function populateProducts() {
+        const products = await fetchProducts();
+        const productList = document.getElementById("productList");
+        products.forEach(product => {
+            const productItem = document.createElement("a");
+            productItem.target = "_blank";
+            productItem.classList.add("product-item", "text-decoration-none", "d-inline-block");
+            productItem.href = "productDetails.html";
+
+            const productImage = document.createElement("div");
+            const productName = document.createElement("div");
+            const productPrice = document.createElement("div");
+
+            productImage.classList.add("product-img");
+            productName.classList.add("product-name", "text-center");
+            productPrice.classList.add("product-price", "text-center");
+
+            productName.textContent = product.title;
+            productPrice.textContent = `&#8377; ${product.price}`;
+
+            const imageInsideProductImage = document.createElement("img");
+            imageInsideProductImage.src = product.image;
+
+            //append divs
+            productImage.appendChild(imageInsideProductImage);
+            productItem.appendChild(productImage);
+            productItem.appendChild(productName);
+            productItem.appendChild(productPrice);
+            productList.appendChild(productItem);
+
+        });
+    }
+    populateProducts();
 });
 
