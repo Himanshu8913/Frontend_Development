366004beb8ed48cd9997bae513e522135be43157
Himanshu Gupta <himanshugupta558@gmail.com>
2024-05-30 01:15:51 +0530

    added board logic and toastify

--- React/react_projects/src/TicTacToe_Project/components/Card/Card.jsx
+++ React/react_projects/src/TicTacToe_Project/components/Card/Card.jsx
@@ -1,14 +1,17 @@
 import Icon from "../Icon/Icon";
 import './Card.css';
 
-function Card({ iconName, onPlay }) {
+function Card({ onPlay, player, index, gameEnd }) {
 
-    function playMove() {
-        onPlay();
+    let icon = <Icon />
+    if (player == "X") {
+        icon = <Icon name={"cross"} />
+    } else if (player == "O") {
+        icon = <Icon name={"circle"} />
     }
     return (
-        <div className="card" onClick={playMove}>
-            <Icon name={iconName} />
+        <div className="card" onClick={() => !gameEnd && player == "" && onPlay(index)}>
+            {icon}
         </div>
     )
 }
